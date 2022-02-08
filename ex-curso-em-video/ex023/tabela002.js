77b0c8ff0749d1780df139b4194f13a052b2f89a
Wandersonfv <wandersonfv10@gmail.com>
2022-02-08 10:50:42 -0300

    correção tabela002.js

--- ex-curso-em-video/ex023/tabela002.js
+++ ex-curso-em-video/ex023/tabela002.js
@@ -1,5 +1,16 @@
-document.querySelector('body').onload = function(){
-    var nun = document.querySelectorAll('td.nun').innerHTML
+
+var td = document.querySelectorAll('td.nun')
+var total = document.querySelector('td#total')
+
+var tot = 0
+for(var n=0; n< td.length; n++){
+    console.log(Number(td[n].innerHTML))
+    tot = Number(td[n].innerHTML) + tot
     
-    console.log(nun)
 }
+console.log('---------------------')
+console.log(tot)
+total.innerHTML = `${tot}`
+
+console.log('---------------------')
+console.log(Number(td[26].innerHTML) + Number(td[26].innerHTML))
